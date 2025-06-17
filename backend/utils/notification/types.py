113e9362679601f0c32d48e912c02a35f7787b87
Khant Hmue <khanthmu152uit@gmail.com>
2025-06-17 11:45:36 +0630

    fix-job-post-filter

---
+++ backend/utils/notification/types.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class NotificationType(str, Enum):
+     JOB_POSTED = "job_posted"
+     JOB_APPLIED = "job_applied"
+     COMPANY_APPROVED = "company_approved"
+
+class NotificationChannel(str, Enum):
+     EMAIL = "email"
+     WEBSOCKET = "websocket"
+     BOTH = "both"
