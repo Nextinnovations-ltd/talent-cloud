6570b514d9f5bd17f3c51fa03e91c2992a9cc0d7
Khant Hmue <khanthmu152uit@gmail.com>
2025-06-18 14:08:53 +0630

    add-bookmark-flag-for-job-details-and-listing

--- backend/main/config/urls.py
+++ backend/main/config/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.http import HttpResponse
-from rest_framework.routers import DefaultRouter
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 def default_endpoint(request):
