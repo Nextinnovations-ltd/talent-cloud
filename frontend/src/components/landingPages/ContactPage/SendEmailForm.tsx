2a385296c8f7512a23ff49a7bf74bd792de491ea
Thura69 <spyking64@gmail.com>
2025-08-22 16:00:07 +0630

    update(contact)

--- frontend/src/components/landingPages/ContactPage/SendEmailForm.tsx
+++ frontend/src/components/landingPages/ContactPage/SendEmailForm.tsx
@@ -7,43 +7,45 @@ import InputField from '@/components/common/form/fields/input-field'
 import { Button } from '@/components/ui/button'
 import TextAreaField from '@/components/common/form/fields/text-area-field'
 
- const SendEmailForm = () => {
+const SendEmailForm = () => {
 
     const form = useForm({
-        resolver:yupResolver(SendEmailSchema)
+        resolver: yupResolver(SendEmailSchema)
     });
 
-    const handleSubmit = async (value)=>{
-    console.log(value)
+    const handleSubmit = async (value) => {
+        console.log(value)
     }
 
-  return (
-    <div  className='border-2 w-full'>
-        <Form {...form} >
-            <form  onSubmit={form.handleSubmit(handleSubmit)} >
-                <InputField 
-                fieldName={'name'} 
-                lableName='Name'
-                placeholder='Name' 
-                languageName={''} 
-                required={false}
-                fieldWidth='w-[200px]'
-                />
+    return (
+        <div className='border-2 w-full'>
+            <Form {...form} >
+                <form onSubmit={form.handleSubmit(handleSubmit)} >
+                    <InputField
+                        fieldName={'name'}
+                        lableName='Name'
+                        placeholder='Name'
+                        languageName={''}
+                        required={false}
+                        //styling here
+                        fieldWidth='w-[200px]'
+                    />
 
-<TextAreaField 
-                fieldName={'description'} 
-                lableName='Description'
-                placeholder='Description' 
-                languageName={''} 
-                required={false}
-                fieldWidth='w-[200px]'
-                />
+                    <TextAreaField
+                        fieldName={'description'}
+                        lableName='Description'
+                        placeholder='Description'
+                        languageName={''}
+                        required={false}
+                         //styling here
+                        fieldWidth='w-[200px]'
+                    />
 
-                <Button className='bg-bg-primary mt-[50px]' type='submit'>Send email</Button>
-            </form>
-        </Form>
-    </div>
-  )
+                    <Button className='bg-bg-primary mt-[50px]' type='submit'>Send email</Button>
+                </form>
+            </Form>
+        </div>
+    )
 }
 
 export default SendEmailForm
