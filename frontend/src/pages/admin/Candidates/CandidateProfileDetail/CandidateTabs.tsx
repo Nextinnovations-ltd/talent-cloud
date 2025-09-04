5a9ecb22f99b076f94de76c28f98bc7f7c624bb3
Thura69 <spyking64@gmail.com>
2025-09-04 08:19:21 +0630

    update(bugs)

--- frontend/src/pages/admin/Candidates/CandidateProfileDetail/CandidateTabs.tsx
+++ frontend/src/pages/admin/Candidates/CandidateProfileDetail/CandidateTabs.tsx
@@ -32,15 +32,15 @@ const CandidateTabs = () => {
 
     return (
         <div className="flex w-full mt-[100px] pb-[100px] flex-col gap-6">
-            <Tabs value={value} onValueChange={setValue} defaultValue="overview" className="w-full h-full">
-                <TabsList className="gap-[24px] flex flex-wrap">
+            <Tabs value={value} onValueChange={setValue} defaultValue="overview" className="w-full  h-full">
+                <TabsList className="gap-[5px] flex flex-wrap">
                     {TABSDATAS.map((tab) => {
                         const Icon = tab.Icon;
                         return (
                             <TabsTrigger
                                 key={tab.id}
                                 value={tab.value}
-                                className="gap-2 px-[24px] py-[12px] flex items-center"
+                                className="gap-2 px-[24px]   py-[12px] flex items-center"
                             >
                                 <Icon color={value === tab.value ? "#0481EF" : "#575757"} />
                                 <p>{tab.name}</p>
