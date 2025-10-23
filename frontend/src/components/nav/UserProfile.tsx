090e7b0b766988465661df1c0d0f736cf3b0973f
Thura69 <spyking64@gmail.com>
2025-10-23 15:49:06 +0630

    ✨ Fix issues
    fix isscues

--- frontend/src/components/nav/UserProfile.tsx
+++ frontend/src/components/nav/UserProfile.tsx
@@ -46,7 +46,7 @@ export const UserProfile = () => {
   };
 
   return (
-    <div className='relative flex items-center gap-2' ref={dropdownRef}>
+    <div className='relative flex items-center gap-2 ' ref={dropdownRef}>
       <Link className='flex  items-center gap-3' to={`user/${routesMap?.profile?.path}`}>
         <AvatarProfile src={userData?.profile_image_url} />
       </Link>
